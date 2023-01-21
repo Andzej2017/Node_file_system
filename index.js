cb132a79d93b274af8873a7e77b4495539fec510
Andzej2017 <andrzej.j2007@yahoo.com>
2023-01-21 22:23:49 +0200

    create file function

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-import { file } from "./file.js";
+import { file } from "./lib/file.js";
+
+const person = {
+    name: 'Somebody'
+};
+const status = await file.create('users', 'person.json', person);
+console.log(status)
+file.read();
