4bc01b62112a5501edef41fbfd4065ab7690e344
Andzej2017 <andrzej.j2007@yahoo.com>
2023-01-22 23:29:30 +0200

    file delete and update functions

--- index.js
+++ index.js
@@ -9,15 +9,20 @@ console.log(createFileStatus);
 const readFileStatus1 = await file.read('users','person.json');
 console.log(readFileStatus1);
 
-/*file.update();
+
+const person2 = {
+    name: 'Somebody2',
+    age: 7
+};
+const updateFileStatus1 = await file.update('users','person.json', person2);
+console.log(updateFileStatus1);
 
 const readFileStatus2 = await file.read('users','person.json');
 console.log(readFileStatus2);
 
-file.delete();
+const deleteFileStatus1 = await file.delete('users','person.json');
+console.log(deleteFileStatus1);
 
 const readFileStatus3 = await file.read('users','person.json');
 console.log(readFileStatus3);
-
-*/
 
