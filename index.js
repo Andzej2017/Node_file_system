0689a30c564ad0198042c6c321698062f5217e78
Andzej2017 <andrzej.j2007@yahoo.com>
2023-01-23 22:37:15 +0200

    Some function tests

--- index.js
+++ index.js
@@ -26,3 +26,30 @@ console.log(deleteFileStatus1);
 const readFileStatus3 = await file.read('users','person.json');
 console.log(readFileStatus3);
 
+const users = [
+    { name: 'Test1', age: 99, isStudent: true },
+    { name: 'Test2', age: 1, isStudent: false },
+    { name: 'Test3', age: 6, isStudent: false },
+];
+
+for(const user of users){
+    const fileName = user.name.toLowerCase() + '.json';
+    const status = await file.create('users', fileName, user);
+    console.log(fileName, status);
+}
+
+const status2 = await file.create('users', 'allusers.json', users);
+console.log(status2, users);
+
+const deleteFileStatus2 = await file.delete('users','test1.json');
+console.log(deleteFileStatus2);
+
+const [fileStatus, fileInfo] = await file.read('users','test2.json');
+if(!fileStatus){
+    const fileInfoObject = JSON.parse(fileInfo);
+    const userName = fileInfoObject.name ? fileInfoObject.name : 'Test10';
+    if(userName === 'Test10'){
+        await file.delete('users','test2.json');   
+    }
+}
+
